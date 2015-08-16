e92275951be3c1f1eb1632354eb808fd76350965
Lailla <Lailla@LAILLA-PC>
2015-08-15 21:57:17 -0300

    leitura

---
+++ AnaliseArtigosIA/src/analiseartigosia/AnaliseArtigosIA.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analiseartigosia;
+
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+/**
+ *
+ * @author Lailla
+ */
+public class AnaliseArtigosIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+
+        String caminho = "arquivos/1.pdf";
+        String texto = extraiTextoDoPDF(caminho);
+        System.out.println(texto);
+
+    }
+
+    public static String extraiTextoDoPDF(String caminho) throws IOException {
+        PDDocument pdfDocument = null;
+        pdfDocument = PDDocument.load(caminho);
+        PDFTextStripper stripper = new PDFTextStripper();
+        String texto = stripper.getText(pdfDocument);
+        return texto;
+    }
+
+}
