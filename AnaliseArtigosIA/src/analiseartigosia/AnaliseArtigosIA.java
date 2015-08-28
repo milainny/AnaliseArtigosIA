719597f0592d1f861ff06d2f7076a66fde46e032
Lailla <Lailla@LAILLA-PC>
2015-08-27 21:31:25 -0300

    tentativa objetivos

--- AnaliseArtigosIA/src/analiseartigosia/AnaliseArtigosIA.java
+++ AnaliseArtigosIA/src/analiseartigosia/AnaliseArtigosIA.java
@@ -18,6 +18,7 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.Set;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.util.PDFTextStripper;
 import org.apache.pdfbox.util.TextPosition;
 import sun.awt.FontDescriptor;
@@ -32,7 +33,7 @@ public class AnaliseArtigosIA {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        String caminhoEntrada = "arquivos/4.pdf";
+        String caminhoEntrada = "arquivos/12.pdf";
         String identificador = caminhoEntrada.substring(caminhoEntrada.lastIndexOf("/") + 1, caminhoEntrada.lastIndexOf("."));
         String caminhoArquivoStopWords = "stop_words/lista_stop_words.txt";
         String caminhoSaidaSemStopWords = "saidas/" + identificador + ".txt";
@@ -40,7 +41,8 @@ public class AnaliseArtigosIA {
         String textoSemStopWords = extraiStopWords(textoCompleto, caminhoArquivoStopWords);
         gravaTextoSemStopWords(caminhoSaidaSemStopWords, textoSemStopWords);
         exibeTermosMaisCitados(textoSemStopWords);
-        identificaInstituicoes(textoCompleto);
+        identificaInstituicoes(caminhoEntrada);
+        identificaObjetivo(caminhoEntrada);
     }
 
     public static String extraiTextoDoPDF(String caminho) throws IOException {
@@ -48,21 +50,17 @@ public class AnaliseArtigosIA {
         pdfDocument = PDDocument.load(caminho);
         PDFTextStripper stripper = new PDFTextStripper();
         String texto = stripper.getText(pdfDocument);
-        PrintStream saidaPadrao = System.out;
-        PrintStream saida = new PrintStream(new FileOutputStream(new File("saidas/texto" + 19 + ".txt")));
-        System.setOut(saida);
-        System.out.println(texto);
-        System.setOut(saidaPadrao);
+//        PrintStream saidaPadrao = System.out;
+//        PrintStream saida = new PrintStream(new FileOutputStream(new File("saidas/texto" + 19 + ".txt")));
+//        System.setOut(saida);
+//        System.out.println(texto);
+//        System.setOut(saidaPadrao);
         pdfDocument.close();
         return texto;
     }
 
     public static String extraiStopWords(String textoCompleto, String caminhoArquivoStopWords) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileReader(caminhoArquivoStopWords));
-        ArrayList<String> listaStopWords = new ArrayList();
-        while (scanner.hasNext()) {
-            listaStopWords.add(scanner.next());
-        }
+        ArrayList<String> listaStopWords = preecheLista(caminhoArquivoStopWords);
         String textoCompletoMin = textoCompleto.toLowerCase();
         textoCompletoMin = textoCompletoMin.replace("\r", "");
         textoCompletoMin = textoCompletoMin.replaceAll("\\W", " ");
@@ -112,41 +110,123 @@ public class AnaliseArtigosIA {
         }
     }
 
-    private static void identificaInstituicoes(String textoCompleto) {
+    private static void identificaInstituicoes(String caminho) throws IOException {
+        String textoPrimeirasPaginas = leitorDePaginas(caminho, 2);
+
+//        PrintStream saidaPadrao = System.out;
+//        PrintStream saida = new PrintStream(new FileOutputStream(new File("saidas/texto2pg" + 12 + ".txt")));
+//        System.setOut(saida);
+//        System.out.println(textoPrimeirasPaginas);
+//        System.setOut(saidaPadrao);
+        String caminhoArquivoInstuicoes = "stop_words/instituicoes.txt";
         String instituicoes = "";
-        int posicaoAbs = textoCompleto.lastIndexOf("Abstract");
-        String textoBusca = textoCompleto;
-        if (posicaoAbs != -1) {
-            textoBusca = textoCompleto.substring(0, textoCompleto.lastIndexOf("Abstract"));
-        }
-        int posicao = textoBusca.indexOf("Department");
-        if (posicao == -1) {
-            if (instituicoes.equals("")) {
-                textoBusca = textoCompleto;
-                posicao = textoBusca.indexOf("Department");
+        ArrayList<String> listaInstiuicoes = preecheLista(caminhoArquivoInstuicoes);
+        for (String inst : listaInstiuicoes) {
+            String textoBusca = textoPrimeirasPaginas;
+            int posicaoAbs = textoPrimeirasPaginas.lastIndexOf("Abstract");
+            if (posicaoAbs != -1) {
+                textoBusca = textoPrimeirasPaginas.substring(0, textoPrimeirasPaginas.lastIndexOf("Abstract"));
             }
-        }
-        while (posicao != -1) {
-            while (true) {
-                if ("\n".equals(textoBusca.substring(posicao, posicao + 1))) {
-                    break;
+            int posicao = textoBusca.indexOf(inst);
+            if (posicao == -1) {
+                if (instituicoes.equals("")) {
+                    textoBusca = textoPrimeirasPaginas;
+                    posicao = textoBusca.indexOf(inst);
                 }
-                posicao--;
             }
-            posicao++;
-            int posInicio = posicao;
-            while (true) {
-                if ("\n".equals(textoBusca.substring(posicao, posicao + 1))) {
-                    break;
-                }
-                posicao++;
+            while (posicao != -1) {
+                posicao = buscaInicioParagrafo(textoBusca, posicao, "\n");
+                int posInicio = posicao;
+                posicao = buscaFimParagrafo(textoBusca, posicao, "\n");
+                instituicoes = instituicoes + textoBusca.substring(posInicio, posicao) + "\n ";
+                textoBusca = textoBusca.substring(posicao, textoBusca.length());
+                posicao = textoBusca.indexOf(inst);
             }
-            instituicoes = instituicoes + textoBusca.substring(posInicio, posicao) + "\n ";
-            textoBusca = textoBusca.substring(posicao, textoBusca.length());
-            posicao = textoBusca.indexOf("Department");
         }
         System.out.println("\n--- Listando as instituições dos autores ---");
         System.out.println(instituicoes);
+    }
+
+    private static int buscaFimParagrafo(String textoBusca, int posicao, String operador) {
+        while (true) {
+            if (operador.equals(textoBusca.substring(posicao, posicao + 1))) {
+                break;
+            }
+            posicao++;
+        }
+        return posicao;
+    }
+
+    private static int buscaInicioParagrafo(String textoBusca, int posicao, String operador) {
+        while (true) {
+            if (operador.equals(textoBusca.substring(posicao, posicao + 1))) {
+                break;
+            }
+            posicao--;
+        }
+        posicao++;
+        return posicao;
+    }
+
+    private static ArrayList<String> preecheLista(String caminhoArquivo) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new FileReader(caminhoArquivo));
+        ArrayList<String> lista = new ArrayList();
+        while (scanner.hasNext()) {
+            lista.add(scanner.next());
+        }
+        return lista;
+    }
+    
+        private static ArrayList<String> preecheListaEDivide(String caminhoArquivo) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new FileReader(caminhoArquivo));
+        String arquivo="";
+        ArrayList<String> lista = new ArrayList();
+        while (scanner.hasNext()) {
+            arquivo += scanner.next()+ " ";
+        }
+        String[] resultado = arquivo.split(" - ");
+            for (String s : resultado) {
+                lista.add(s.trim());
+            }
+        return lista;
+    }
+
+    private static String leitorDePaginas(String caminho, int quantidadePaginas) throws IOException {
+        PDDocument pdfDocument = null;
+        pdfDocument = PDDocument.load(caminho);
+        PDFTextStripper stripper = new PDFTextStripper();
+        stripper.setEndPage(quantidadePaginas);
+        String textoPrimeirasPaginas = stripper.getText(pdfDocument);
+        pdfDocument.close();
+        return textoPrimeirasPaginas;
+    }
+
+    private static void identificaObjetivo(String caminho) throws IOException {
+        String textoPrimeirasPaginas = leitorDePaginas(caminho, 5);
+        textoPrimeirasPaginas = textoPrimeirasPaginas.toLowerCase();
+        String caminhoArquivoObjetivo = "stop_words/objetivo.txt";
+        ArrayList<String> listaObjetivo = preecheListaEDivide(caminhoArquivoObjetivo);
+        ArrayList<String> naoReptir = new ArrayList<>();
+
+        String resultado = "";
+        for (String obj : listaObjetivo) {
+            int posicao = textoPrimeirasPaginas.indexOf(obj);
+            if (posicao != -1) {
+                posicao = buscaInicioParagrafo(textoPrimeirasPaginas, posicao, ".");
+                int posInicio = posicao;
+                posicao = buscaFimParagrafo(textoPrimeirasPaginas, posicao, ".");
+                resultado = textoPrimeirasPaginas.substring(posInicio, posicao);
+                if (!naoReptir.contains(resultado)) {
+                    naoReptir.add(resultado);
+                }
+            }
+        }
+        System.out.println("\n--- Listando o objetivo do artigo ---");
+        for (String s : naoReptir) {
+            System.out.println(s);
+            System.out.println("");
+        }
+        
     }
 
 }
